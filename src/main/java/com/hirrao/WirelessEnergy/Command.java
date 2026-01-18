f6517e5c5ee15ebd8895e111b67d3cb803c6b04f
TCmanna <wen1748@126.com>
2026-01-18 18:50:28 +0800

    更改为无权限命令 格式化能量显示 修复能量不同步问题。

--- src/main/java/com/hirrao/WirelessEnergy/Command.java
+++ src/main/java/com/hirrao/WirelessEnergy/Command.java
@@ -1,7 +1,9 @@
 package com.hirrao.WirelessEnergy;
 
 import net.minecraft.command.*;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
@@ -21,44 +23,74 @@ public class Command extends CommandBase {
 
     @Override
     public String getUsage(ICommandSender sender) {
-        return "/energy <user>";
+        return "/energy [user]";
+    }
+
+    @Override
+    public int getRequiredPermissionLevel() {
+        return 0;
     }
 
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-        if (args.length < 1) {
-            throw new WrongUsageException(getUsage(sender));
-        }
-        String playerName = args[0];
-        EntityPlayer player;
-        try {
-            player = getPlayer(server, sender, playerName);
-        } catch (PlayerNotFoundException e) {
-            sender.sendMessage(new TextComponentString("No player found with name: " + playerName));
-            return;
-        }
-        UUID uuid = player.getUniqueID();
         World world = sender.getEntityWorld();
         if (world.isRemote) {
             sender.sendMessage(new TextComponentString("This command can only be run on the server."));
             return;
         }
+
+        EntityPlayer player;
+
+        if (args.length < 1) {
+            if (sender instanceof EntityPlayerMP) player = (EntityPlayerMP) sender;
+            else {
+                sender.sendMessage(new TextComponentString("No player found"));
+                return;
+            }
+        } else {
+            String playerName = args[0];
+            try {
+                player = getPlayer(server, sender, playerName);
+            } catch (PlayerNotFoundException e) {
+                sender.sendMessage(new TextComponentString("No player found with name: " + playerName));
+                return;
+            }
+        }
+        UUID uuid = player.getUniqueID();
         try {
             BigInteger energyNow = totalEnergyNow.getOrDefault(uuid, BigInteger.ZERO);
             BigInteger energyLast = totalEnergyLast.getOrDefault(uuid, BigInteger.ZERO);
             BigInteger energyDiff = energyNow.subtract(energyLast);
             Log.LOGGER.info("energyNow: " + energyNow + ", energyLast: " + energyLast + ", energyDiff: " + energyDiff);
-            sender.sendMessage(new TextComponentString("无线电网存储能量 " + energyNow + " EU"));
+            sender.sendMessage(new TextComponentString("无线电网存储能量 " + String.format("%,d", energyNow) + " EU"));
             if (energyDiff.compareTo(BigInteger.ZERO) >= 0) {
-                sender.sendMessage(new TextComponentString("平均输入" + energyDiff.divide(BigInteger.valueOf(20)) + " EU/t"));
+                sender.sendMessage(new TextComponentString("平均输入" + String.format("%,d", energyDiff.divide(BigInteger.valueOf(20))) + " EU/t"));
             } else if (energyDiff.compareTo(BigInteger.ZERO) < 0) {
-                sender.sendMessage(new TextComponentString("平均输出" + energyDiff.abs().divide(BigInteger.valueOf(20)) + " EU/t"));
+                sender.sendMessage(new TextComponentString("平均输出" + String.format("%,d", energyDiff.abs().divide(BigInteger.valueOf(20))) + " EU/t"));
                 BigInteger time = energyNow.divide(energyDiff.abs());
-                sender.sendMessage(new TextComponentString("预计" + time + "秒后能量耗尽"));
+                sender.sendMessage(new TextComponentString("预计" + formatDuration(time) + "后能量耗尽"));
             }
         } catch (Exception e) {
             Log.LOGGER.error(e);
         }
 
+    }
+
+    private static String formatDuration(BigInteger time) {
+        BigInteger[] dr = time.divideAndRemainder(BigInteger.valueOf(86400));
+        BigInteger days = dr[0];
+        BigInteger[] hr = dr[1].divideAndRemainder(BigInteger.valueOf(3600));
+        BigInteger hours = hr[0];
+        BigInteger[] mr = hr[1].divideAndRemainder(BigInteger.valueOf(60));
+        BigInteger minutes = mr[0];
+        BigInteger seconds = mr[1];
+        StringBuilder sb = new StringBuilder();
+
+        if (days.signum() > 0) sb.append(days).append("天");
+        if (hours.signum() > 0) sb.append(hours).append("小时");
+        if (minutes.signum() > 0) sb.append(minutes).append("分钟");
+        sb.append(seconds).append("秒");
+
+        return sb.toString();
     }
 }
--- src/main/java/com/hirrao/WirelessEnergy/EventTick.java
+++ src/main/java/com/hirrao/WirelessEnergy/EventTick.java
@@ -9,7 +9,6 @@ import java.util.HashMap;
 import java.util.UUID;
 
 public class EventTick {
-    private static final int TICKS_PER_SECOND = 20;
     public static HashMap<UUID, BigInteger> totalEnergyNow;
     public static HashMap<UUID, BigInteger> totalEnergyLast;
     private int tickCount = 0;
@@ -17,18 +16,19 @@ public class EventTick {
     @SubscribeEvent
     public void onServerTick(TickEvent.ServerTickEvent event) {
         if (event.phase == TickEvent.Phase.END) {
-            tickCount++;
-            if (tickCount >= TICKS_PER_SECOND) {
-                tickCount = 0;
+            tickCount = (tickCount + 1) % 20;
+            if (tickCount == 0) {
                 try {
                     Class<?> clazz = Class.forName("magicbook.gtlitecore.api.misc.GlobalVariableStorage");
                     Field field = clazz.getDeclaredField("WirelessEnergy");
                     HashMap<UUID, BigInteger> wirelessEnergy = (HashMap<UUID, BigInteger>) field.get(null);
                     if (totalEnergyNow == null) {
-                        totalEnergyNow = wirelessEnergy;
+                        totalEnergyNow = new HashMap<>(wirelessEnergy);
+                        totalEnergyLast = new HashMap<>(wirelessEnergy);
+                        return;
                     }
                     totalEnergyLast = new HashMap<>(totalEnergyNow);
-                    totalEnergyNow = wirelessEnergy;
+                    totalEnergyNow = new HashMap<>(wirelessEnergy);
                 } catch (NoSuchFieldException | ClassNotFoundException e) {
                     Log.LOGGER.error("Failed to get WirelessEnergy from GlobalVariableStorage");
                 } catch (Exception e) {
